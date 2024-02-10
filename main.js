61d06b2afbee285c2da8498953d34ff63ecdf758
elviredev <sandrine.rodriguez@hotmail.com>
2024-02-10 14:22:55 +0100

    Work in progress, fetched pets data

--- main.js
+++ main.js
@@ -26,3 +26,16 @@ async function start(){
 }
 
 start()
+
+/*
+ * API data pets.json
+ */
+async function petsArea(){
+    const petsPromise = await fetch("https://learnwebcode.github.io/bootcamp-pet-data/pets.json")
+    const petsData = await petsPromise.json()
+    petsData.forEach(pet => {
+        console.log(pet.name)
+    })
+}
+
+petsArea()
